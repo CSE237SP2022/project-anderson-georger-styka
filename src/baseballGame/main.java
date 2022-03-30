85b37acd0db1b1d188c0caf54ecbd688b3458668
joewgeorger <59860417+joewgeorger@users.noreply.github.com>
2022-03-30 11:16:10 -0500

    Streamlined2

--- src/baseballGame/main.java
+++ src/baseballGame/main.java
@@ -4,47 +4,6 @@ public class main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-<<<<<<< HEAD
-		Game play;
-		double min = 0.0
-		double max = 1.1
-		int holder;
-		
-		while(play.getTurnNum() < 5) {
-			double randomNumber = (double)Math.floor(Math.random()*(max-min+1)+min);
-				if(randomNumber < 0.7) {
-					holder = 0;
-				}
-				else if(randomNumber < 0.8) {
-					holder = 1;
-				}
-				else if(randomNumber < 0.9) {
-					holder = 2;
-				}
-				else if(randomNumber < 1.0) {
-					this.score = 3;
-				}
-				else if(randomNumber > 1.0 && randomNumber < 1.1) {
-					holder = 4;
-				}
-				else {
-					holder = 0;
-				}
-			if(play.getPlayerTurn()) {
-				play.updatePlay1Score(holder);
-				//System.out.print(getScore)
-				play.nextTurn();
-			}
-			else{
-				play.updatePlay2Score(holder);
-				//System.out.print(getScore)
-				play.nextTurn();
-			}
-		}
-			System.out.print("Player 1 Score =" + play.getPlayer1Score() + "Player 2 Score =" + play.getPlayer2Score());
-
-		}
-=======
 		Game play = new Game();
 		
 		while(play.getTurnNum() < 5) {
@@ -60,7 +19,6 @@ public class main {
 				}
 				System.out.print("Turn: " + play.getTurnNum() + "; Player 1 Score = " + play.getPlayer1Score() + "; Player 2 Score = " + play.getPlayer2Score() + "\n");
 			}
->>>>>>> d37135e (Adjusted Main)
 	}
 
 }
