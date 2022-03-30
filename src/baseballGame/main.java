cbbec9a28caa1241fabb7503e2d6d76593f2d141
joewgeorger <joewgeorger@wustl.edu>
2022-03-29 22:55:22 -0500

    little changes

--- src/baseballGame/main.java
+++ src/baseballGame/main.java
@@ -4,8 +4,13 @@ public class main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		Game play;
-		
+		Game play = new Game();
+		while(play.getTurnNum() < 5) {
+			//I need to add a buffer or scanner to get input
+			//get input
+			
+			
+		}
 	}
 
 }
